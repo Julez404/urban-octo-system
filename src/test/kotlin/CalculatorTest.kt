a2febf199e6cbd11e873be5337c2c437d39191b6
yoshi <julian404@icloud.com>
2021-05-28 10:00:19 +0200

    Let 1 Test fail for CI testing

--- src/test/kotlin/CalculatorTest.kt
+++ src/test/kotlin/CalculatorTest.kt
@@ -16,7 +16,7 @@ public class CalculatorTest {
     fun sumOfNegativeNumbers() {
         val calculator = Calculator()
 
-        assertEquals(-4, calculator.add(-2, -2))
+        assertEquals(4, calculator.add(-2, -2))
         assertEquals(-266, calculator.add(-10, -256))
         assertEquals(-9000, calculator.add(-0, -9000))
         assertEquals(-180000, calculator.add(-90000, -90000))
