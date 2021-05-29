733552adf98d76e627bdb8e30e4048a48ad631d1
yoshi <julian404@icloud.com>
2021-05-29 10:00:32 +0200

    Remove unused Exceptions

--- src/main/kotlin/Instruction.kt
+++ src/main/kotlin/Instruction.kt
@@ -40,8 +40,5 @@ class Instruction(initString: String) {
 
     inner class InvalidInstructionException : Exception {
         constructor() : super()
-        constructor(message: String) : super(message)
-        constructor(message: String, cause: Throwable) : super(message, cause)
-        constructor(cause: Throwable) : super(cause)
     }
 }
